8c631c3fd4b03b1b244433aa98254afc1654a93f
ullash-tanvir <tanvirbracu16@gmail.com>
2020-12-15 01:03:46 +0600

    first

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDVxTI5zq3whvWHHktKjYgyzBHj-G3WCvc",
+    authDomain: "project-303b0.firebaseapp.com",
+    databaseURL: "https://project-303b0-default-rtdb.firebaseio.com",
+    projectId: "project-303b0",
+    storageBucket: "project-303b0.appspot.com",
+    messagingSenderId: "737019178941",
+    appId: "1:737019178941:web:602083e068763f40e4acd4",
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDVxTI5zq3whvWHHktKjYgyzBHj-G3WCvc",
+    authDomain: "project-303b0.firebaseapp.com",
+    databaseURL: "https://project-303b0-default-rtdb.firebaseio.com",
+    projectId: "project-303b0",
+    storageBucket: "project-303b0.appspot.com",
+    messagingSenderId: "737019178941",
+    appId: "1:737019178941:web:602083e068763f40e4acd4"
+  },
+};
